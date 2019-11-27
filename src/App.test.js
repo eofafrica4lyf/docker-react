1b3820fce068748d671b18d83b42ee34d423f0c3
Emmanuel Aboderin <aboderinemmanuel@gmail.com>
2019-11-27 12:45:54 +0100

    demonstrate the concept of volumes; add docker-compose file to easily run docker commands

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { equal } from 'assert';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+  expect(1).toBe(1)
 });
